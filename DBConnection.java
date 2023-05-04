bd4a35d1470ff32a9336d64ba165ea6bb9bf1418
jorgelosada1 <126262020+jorgelosada1@users.noreply.github.com>
2023-05-04 16:22:44 -0500

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,31 @@
+package co.edu.sena.project2687351.util;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL =
+            "jdbc:mysql://127.0.0.1:3306/?user=root";
+    private static final String USER = "my_app_user";
+    private static final String PASS = "my_app_password";
+    private static BasicDataSource pool;
+    public static  BasicDataSource getInstance() throws SQLException {
+        if (pool == null){
+            pool = new BasicDataSource();
+            pool.setUrl(URL);
+            pool.setUsername(USER);
+            pool.setPassword(PASS);
+
+            pool.setInitialSize(5);
+            pool.setMinIdle(5);
+            pool.setMaxIdle(10);
+            pool.setMaxTotal(10);
+        }
+        return pool;
+    }
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
+    }
+}
