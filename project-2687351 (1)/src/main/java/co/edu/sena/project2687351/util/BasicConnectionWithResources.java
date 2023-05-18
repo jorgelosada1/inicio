79a671e778b4ca961a9c8ad868a2730dc15a3ee4
jorgelosada1 <126262020+jorgelosada1@users.noreply.github.com>
2023-05-18 15:42:21 -0500

    Update BasicConnectionWithResources.java

--- project-2687351 (1)/src/main/java/co/edu/sena/project2687351/util/BasicConnectionWithResources.java
+++ project-2687351 (1)/src/main/java/co/edu/sena/project2687351/util/BasicConnectionWithResources.java
@@ -6,10 +6,10 @@ public class BasicConnectionWithResources
 {
     public static void main(String[] args)
     {
-        String url = "jdbc:mysql://localhost:3306/#?serve rTimezone=America/Bogota";
-        String username = "#";
-        String password = "#";
-        String sql = "SELECT * FROM my_app.users";
+        String url = "jdbc:mysql://localhost:3306/Login?serverTimezone=America/Bogota";
+        String username = "root";
+        String password = "";
+        String sql = "SELECT * FROM Login.users";
         try (Connection conn = DriverManager.getConnection(url, username, password);
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql))
