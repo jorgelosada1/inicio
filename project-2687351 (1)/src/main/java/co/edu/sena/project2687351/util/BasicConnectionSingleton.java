a31250ed4c2dd902627e4e9825ad009bf93fe33b
jorgelosada1 <126262020+jorgelosada1@users.noreply.github.com>
2023-05-18 15:41:45 -0500

    Update BasicConnectionSingleton.java

--- project-2687351 (1)/src/main/java/co/edu/sena/project2687351/util/BasicConnectionSingleton.java
+++ project-2687351 (1)/src/main/java/co/edu/sena/project2687351/util/BasicConnectionSingleton.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class BasicConnectionSingleton {
-    private static String url = "jdbc:mysql://localhost:3306/#?serverTimezone=America/Bogota";
-    private static String user = "#";
-    private static String pass = "#";
+    private static String url = "jdbc:mysql://localhost:3306/Login?serverTimezone=America/Bogota";
+    private static String user = "root";
+    private static String pass = "";
     private static Connection conn;
     public static Connection getInstance() throws SQLException {
         if (conn == null) {conn = DriverManager.getConnection(url, user, pass);
