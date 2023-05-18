8ac4d1e230448063feb09e7ccb69b71f36efca84
jorgelosada1 <126262020+jorgelosada1@users.noreply.github.com>
2023-05-18 15:42:48 -0500

    Update DBConnection.java

--- project-2687351 (1)/src/main/java/co/edu/sena/project2687351/util/DBConnection.java
+++ project-2687351 (1)/src/main/java/co/edu/sena/project2687351/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/?user=root";
+    private static final String URL = "jdbc:mysql://localhost:3306/Login?serverTimezone=America/Bogota";
     private static final String USER = "root";
     private static final String PASS = "";
     private static BasicDataSource pool;
