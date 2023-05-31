51b8e959406d28a6dc28295404e8c0312c9e24ca
jorgelosada1 <126262020+jorgelosada1@users.noreply.github.com>
2023-05-31 14:00:18 -0500

    Update taller4.sql

--- taller4.sql
+++ taller4.sql
@@ -9,6 +9,18 @@ user_email    VARCHAR(60) not null,
 user_password  varbinary (255)
 );
 
+create table category (
+category_id integer auto_increment primary key not null,
+category_name varchar(40) not null);
+
+create table products (
+product_id integer not null auto_increment,
+product_name varchar(40) not null, 
+product_value int not null, 
+category_id_fk int not null, 
+foreign key(category_id_fk) references category (category_id), 
+primary key(product_id)) auto_increment;
+
 
 insert into Login.users (user_firstname, user_lastname, user_email, user_password) values (upper ('pedro'), upper ('cortez'), 'email@gmail.com', aes_encrypt('ñero', '$2a$12$kdxJ0X5wdsv6PiVsh65wzeFuLucUaDn23ceJOnLR2UXb9ojGcUnG.'));
 insert into Login.users (user_firstname, user_lastname, user_email, user_password) values (upper ('Mariana'), upper ('Alvarez'), 'giu@gmail.com', aes_encrypt('loviu', '$2a$12$cyYB1dwpDKIwc8DTy9L/..pyz/n5pGuGMQXJONLfI1t.GFcWxnEfC'));
